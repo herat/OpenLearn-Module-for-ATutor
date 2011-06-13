5cc22838a34deffaff2fa5f58130a8297e0d3c15
Herat Gandhi <herat_000@yahoo.co.in>
2011-06-13 16:20:41 +0530

    13th June

--- ol_search_open_learn/result_instructor.php
+++ ol_search_open_learn/result_instructor.php
@@ -7,23 +7,73 @@ require (AT_INCLUDE_PATH.'header.inc.php');
 require ('search.class.php');
 
 $obj = new Search();
-?>	
+?>
+
+<?php
+
+$maxResults = intval(trim(strtolower($_GET['maxResults'])));
+$maxResults1 = intval(trim(strtolower($_GET['maxResults'])));
+
+?>
+
+Maximum Records: 
+<select name="maxResults" id="maxResults" onchange="changeMax()" >
+	<option value="5" <?php if($maxResults==5) echo "selected='selected'" ?>>5</option>
+    <option value="10" <?php if($maxResults==10) echo "selected='selected'" ?>>10</option>
+    <option value="25" <?php if($maxResults==25) echo "selected='selected'" ?>>25</option>
+</select>
+	
+
 <?php 
-$rows = $obj->getSearchResult($_GET['q']);
+
+$start = intval(trim(strtolower($_GET['start'])));
+//$maxResults = intval(trim(strtolower($_GET['maxResults'])));
+
+if ($maxResults == 0) $maxResults = 5;  // default
+
+$rows = $obj->getSearchResult($_GET['q'],$start,$maxResults);
 //echo count($rows)."<br/>";
+
+$all_results = $obj->getSearchResult($_GET['q']);
+
+if (is_array($all_results)) $total_num = count($all_results);
+else $total_num = 0;
+
+// calculate the last record number
+if (is_array($rows))
+{
+	$num_of_results = count($rows);
+	
+	if ($maxResults > $num_of_results) $last_rec_number = $start + $num_of_results;
+	else $last_rec_number = $start + $maxResults;
+}
+else $last_rec_number = $total_num;
+
 if( is_array($rows) && count($rows) > 0) {
-    $i=1;
-	echo "<form name=\"form1\" method=\"post\" action=\"mods/_core/imscp/ims_import.php\" enctype=\"multipart/form-data\" onsubmit=\"openWindow('". AT_BASE_HREF . "tools/prog.php');\">";
+    $i=$start+1;
+	
+	echo "<div id='accordion'>";
+	
     foreach( $rows as $row ) {
 		$importbutton= "<input type=\"submit\" name=\"submit\" onclick=\"setClickSource('submit');\" value='import' />";
-		echo "<input type=\"hidden\" name=\"url\" id=\"to_url\" value='". $row['cc'] ."' />";
-		echo "<input type=\"hidden\" name=\"allow_test_export\" value='1' />";
-        echo $i.". <b><a href='".$row['website']."' target='_new' > ".$row['title']." </a></b>". $importbutton ."<br/>";
 		
-		if( strlen($row['description']) < 300 )
+		echo "<h3>".$i.". <a href='#section". $i ."' > ".$row['title']." </a></h3>";
+		
+		/*if( strlen($row['description']) < 300 )
 			echo $row['description']."<br/>";	
 		else
-        	echo substr($row['description'],0,300)."...<br/>";
+        	echo substr($row['description'],0,300)."...<br/>"; */
+		echo "<div>";
+		echo "<form name=\"form1\" method=\"post\" action=\"mods/_core/imscp/ims_import.php\" enctype=\"multipart/form-data\" onsubmit=\"openWindow('". AT_BASE_HREF . "tools/prog.php');\">";
+		echo "<input type=\"hidden\" name=\"url\" id=\"to_url\" value='". $row['cc'] ."' />";
+		echo "<input type=\"hidden\" name=\"allow_test_export\" value='1' />";
+		echo $importbutton;
+		echo "</form>";
+		
+		
+		echo "<p>".$row['description']."</p>";
+		
+			
         $i++;
 		$imgs = 
 		   "<a href='".$row['cp']."'> 
@@ -34,40 +84,102 @@ if( is_array($rows) && count($rows) > 0) {
             	<img src='mods/ol_search_open_learn/cc.png' 
 				alt='Download Common Cartridge' title='Download Common Cartridge' border='0' /> 
           	</a>";
-		echo "<br/>".$imgs."<br/><br/>";
+		
+		//$prevw = "";	
+		$prevw = "<a href=\"".$row['website']."\" title=\"".$row['title']."\" rel=\"gb_page_fs[]\">Preview on OL</a>";
+			
+		echo "<br/>".$imgs.$prevw;
+			
+		echo "</div>";	
+		
     }
-	echo "</form>";
+	
+	echo "</div>";
+	
+	
+	
+	if( $start > 0 )
+	{
+		$prev = $start-$maxResults;
+		if( $maxResults1 != 0)
+		{
+			echo "<a href='".$_SERVER[PHP_SELF]."?q=".$_GET['q']."&start=".$prev."&maxResults=".$_GET['maxResults']."'><img src='mods/ol_search_open_learn/prev.gif' /></a>&nbsp;&nbsp;&nbsp;&nbsp;";
+		}
+		else
+		{
+			echo "<a href='".$_SERVER[PHP_SELF]."?q=".$_GET['q']."&start=".$prev."'><img src='mods/ol_search_open_learn/prev.gif' /></a>&nbsp;&nbsp;&nbsp;&nbsp;";
+		}
+	}
+	if( $total_num > $last_rec_number )
+	{
+		if( $maxResults1 != 0)
+		{
+			echo "<a href='".$_SERVER[PHP_SELF]."?q=".$_GET['q']."&start=".$last_rec_number."&maxResults=".$_GET['maxResults']."'><img src='mods/ol_search_open_learn/next.gif' /> </a>";
+		}
+		else
+		{
+			echo "<a href='".$_SERVER[PHP_SELF]."?q=".$_GET['q']."&start=".$last_rec_number."'><img src='mods/ol_search_open_learn/next.gif' /> </a>";
+		}
+	}
+	
+	
 }
 else {
-    echo "No results found... for  <b>". $_GET['q'] ."</b><br/>";
+    echo "No results found... for ". $_GET['q'] ." <br/>";
 }
 ?>
-
 <?php
 require (AT_INCLUDE_PATH.'footer.inc.php'); ?>
 
-<script language="javascript" type="text/javascript">
+<script type="text/javascript">
+    var GB_ROOT_DIR = "mods/ol_search_open_learn/greybox/";
+</script>
+<script type="text/javascript" src="mods/ol_search_open_learn/greybox/AJS.js"></script>
 
-var but_src;
-function setClickSource(name) {
-	but_src = name;
-}
+<script type="text/javascript" src="mods/ol_search_open_learn/greybox/gb_scripts.js"></script>
+<link href="mods/ol_search_open_learn/greybox/gb_styles.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="mods/ol_search_open_learn/greybox/AJS_fx.js"></script>
+ 
 
-function openWindow(page) {
-	if (but_src != "cancel") {
-		newWindow = window.open(page, "progWin", "width=400,height=200,toolbar=no,location=no");
-		newWindow.focus();
+<script type="text/javascript" src="jscripts/infusion/lib/jquery/core/js/jquery.js"></script>
+<script type="text/javascript" src="jscripts/infusion/lib/jquery/ui/js/ui.core.js"></script>
+<script type="text/javascript" src="jscripts/infusion/lib/jquery/ui/js/ui.accordion.js"></script>        
+<script type="text/javascript" src="jscripts/infusion/lib/jquery/ui/js/ui.slider.js"></script>  
+<script>
+	$(function() {
+		$( "#accordion" ).accordion({
+			autoHeight: false,
+			navigation: true
+		});
+	});		
+	
+	function changeMax()
+	{
+		var e = document.getElementById("maxResults");
+		var ele= e.options[e.selectedIndex].value;
+		
+		window.location = "<?php echo $_SERVER[PHP_SELF]."?q=".$_GET['q']."&maxResults="; ?>"+ele;
+ 		
 	}
-}
-
-//Change form action 
-function changeFormAction(type){
-	var obj = document.exportForm;
-	if (type=="cc"){
-		obj.action = "mods/_core/imscc/ims_export.php";
-	} else if (type=="cp"){
-		obj.action = "mods/_core/imscp/ims_export.php";
+	var but_src;
+	function setClickSource(name) {
+		but_src = name;
 	}
-}
 
+	function openWindow(page) {
+		if (but_src != "cancel") {
+			newWindow = window.open(page, "progWin", "width=400,height=200,toolbar=no,location=no");
+			newWindow.focus();
+		}
+	}
+
+	//Change form action 
+	function changeFormAction(type){
+		var obj = document.exportForm;
+		if (type=="cc"){
+			obj.action = "mods/_core/imscc/ims_export.php";
+		} else if (type=="cp"){
+			obj.action = "mods/_core/imscp/ims_export.php";
+		}
+	}
 </script>
